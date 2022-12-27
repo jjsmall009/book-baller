56f793c7e59670bbb72d6eebd94576c91e9aa867
JJ Small <jjsmall009@gmail.com>
2022-12-26 20:55:25 -0800

    Frontend - vite and some basic scaffolding, outlining

---
+++ frontend/src/components/BookCard.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import "./bookCardStyle.css";
+
+const BookCard = () => {
+    return (
+        <div className="book-card">
+            <img src="" alt="cover image" />
+            <div className="book-info">
+                <p className="title">The Eye of the World</p>
+                <p className="author">Robert Jordan</p>
+                <p className="description">This is a good book. Read it.</p>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import BookCard from "../components/BookCard";
+
+const Home = () => {
+    return (
+        <main>
+            <h1>Welcome to Book Baller</h1>
+            <p>
+                Book Baller is a simple yet powerful reading tracker web app that allows you to keep
+                track of what they are reading.
+            </p>
+
+            <div className="book-zone">
+                <BookCard />
+                <BookCard />
+            </div>
+        </main>
+    );
+};
+
+export default Home;
