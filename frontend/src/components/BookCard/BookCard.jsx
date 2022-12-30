c11824c35c324bc05ac3da579e21f8b49b498733
JJ Small <jjsmall009@gmail.com>
2022-12-30 00:08:15 +0000

    Restructured the css for now; about page

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import "./bookCardStyle.css";
 
 const BookCard = () => {
     return (
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 // Footer component for all pages
 import { Link } from "react-router-dom";
-import "./Footer.css";
 
 export default function Footer() {
     return (
